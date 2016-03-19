b84a424f4ac02974e888f7fe7ef477ad3d7017cf
Adrian Li <li.adrianmc@gmail.com>
2016-03-19 16:37:30 +0800

    Update 2016-03-19T08:37:27.862Z

---
+++ example.js
@@ -0,0 +1,22 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactRotatingText = require('react-rotating-text');
+
+var App = React.createClass({
+	displayName: 'App',
+
+	render: function render() {
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(ReactRotatingText, null)
+		);
+	}
+});
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
+
+},{"react":undefined,"react-dom":undefined,"react-rotating-text":undefined}]},{},[1]);
